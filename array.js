2ad4ce2890741846579cfbf29c5d8ad7fcba7749
rehamismail9112 <127119149+rehamismail9112@users.noreply.github.com>
2023-03-21 16:38:42 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,20 @@
+var arr = [];
+for (var i = 0; i < 5; i++) {
+  var input = prompt("Enter a number:");
+  arr.push(parseFloat(input));
+}
+
+var arrDesc = arr.slice().sort(function(a, b) { return b - a; });
+
+var arrAsc = arr.slice().sort(function(a, b) { return a - b; });
+
+
+
+
+document.write("Entered values: " + arr.join(", "));
+
+document.write("Array in descending order: " + arrDesc.join(", ") + "<br>");
+
+document.write("Array in ascending order: " + arrAsc.join(", ")); 
+
+window.location.href ="./array.html"
